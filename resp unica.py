ecdd8999e90bb53f37493471935937918530b42b
a-mado <93746906+a-mado@users.noreply.github.com>
2021-11-15 18:19:34 -0500

    Add files via upload

---
+++ resp unica.py
@@ -0,0 +1,18 @@
+class mascota(object):
+    def __init__(self, nombre, especie):
+        self.nombre = nombre
+        self.especie = especie
+
+        def darnombre(self):
+            return self.nombre
+
+        def darespecie(self):
+            return self.especie
+class mascotaDB:
+    def guardarmascotaDB(self,mascota:mascota):
+        pass
+    def elminarmascotaDB(self,mascota:mascota):
+        pass
+    
+
+    
